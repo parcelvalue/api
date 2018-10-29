d90368123c896080bf2e868395ab0938f43bf7f0
Radu Maier <ariademur@webserv.co>
2018-10-29 15:04:20 +0100

    ResourceObject::TYPE_SHIPMENT

--- src/ParcelValue/Api/JsonApi/ResourceObject.php
+++ src/ParcelValue/Api/JsonApi/ResourceObject.php
@@ -1,9 +1,10 @@
 <?php
 namespace ParcelValue\Api\JsonApi;
 
-final class ResourceObject extends \WebServCo\Api\JsonApi\AbstractResourceObject implements
+class ResourceObject extends \WebServCo\Api\JsonApi\AbstractResourceObject implements
     \WebServCo\Api\JsonApi\Interfaces\ResourceObjectInterface
 {
     const TYPE_TEST = 'test';
     const TYPE_CLIENT = 'client';
+    const TYPE_SHIPMENT = 'shipment';
 }

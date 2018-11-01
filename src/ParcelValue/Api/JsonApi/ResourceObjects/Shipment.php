bc19d6b45832e05cda9f26a430a3bea25d6ebfc3
Radu Maier <ariademur@webserv.co>
2018-11-01 15:58:08 +0100

    implement service

--- src/ParcelValue/Api/JsonApi/ResourceObjects/Shipment.php
+++ src/ParcelValue/Api/JsonApi/ResourceObjects/Shipment.php
@@ -5,11 +5,19 @@ class Shipment extends \WebServCo\Api\JsonApi\AbstractResourceObject
 {
     const TYPE = 'shipment';
     const DATE_FORMAT = 'Y-m-d';
+    const SERVICE_ECONOMY = 'economy';
+    const SERVICE_EXPRESS = 'express';
 
     public function __construct($id = null)
     {
         parent::__construct();
         $this->setId($id);
         $this->setType(self::TYPE);
+    }
+
+    public function setService($service)
+    {
+        $this->setMeta('service', $service);
+        return true;
     }
 }

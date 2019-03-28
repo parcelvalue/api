6d4625608d72edb6c6ca0448ba41b49184bd6d74
Radu Maier <ariademur@webserv.co>
2019-03-28 15:16:36 +0100

    API Helper

---
+++ src/ParcelValue/Api/Helper.php
@@ -0,0 +1,52 @@
+<?php
+namespace ParcelValue\Api;
+
+use WebServCo\Api\JsonApi\Document;
+use WebServCo\Framework\Http\Method;
+
+final class Helper
+{
+    protected $logger;
+    protected $curlBrowser;
+
+    public function __construct(
+        \WebServCo\Framework\Interfaces\LoggerInterface $logger,
+        $environment //\WebServCo\Framework\Environment
+    ) {
+        $this->logger = $logger;
+        $this->curlBrowser = new \WebServCo\Framework\CurlBrowser($this->logger);
+        if (\WebServCo\Framework\Environment::ENV_DEV == $environment) {
+            $this->curlBrowser->setSkipSSlVerification(true);
+        }
+        $this->curlBrowser->setRequestHeader('Accept', Document::CONTENT_TYPE);
+    }
+
+    public function getRequestHeaders()
+    {
+        return $this->curlBrowser->getRequestHeaders();
+    }
+
+    /*
+    * @return \WebServCo\Framework\Http\Response
+    */
+    public function getResponse($url, $method, array $headers = [], $requestData = null)
+    {
+        foreach ($headers as $key => $value) {
+            $this->curlBrowser->setRequestHeader($key, $value);
+        }
+        switch ($method) {
+            case Method::POST:
+                $this->curlBrowser->setRequestContentType(Document::CONTENT_TYPE);
+                $this->curlBrowser->setRequestData($requestData);
+                break;
+            case Method::GET:
+            case Method::HEAD:
+                break;
+            default:
+                throw new \WebServCo\Framework\Exceptions\NotImplementedException('Functionality not implemented');
+                break;
+        }
+        $this->curlBrowser->setMethod($method);
+        return $this->curlBrowser->retrieve($url);
+    }
+}
